73393448d32a36eab5bdc43f9eb8b7687383bf53
voduyquocthai <voduyquocthai@gmail.com>
2021-04-17 13:14:10 +0700

    thai

---
+++ src/test/java/com/ryan/emanager/EmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryan.emanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
